efc748872a4eb2ff4d1020838f111c11ea93fd6f
mirzar <mirza@ziked.com>
2021-12-01 14:37:47 -0500

    Day 1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # advent-of-code
+
+To run :
+`cd day`
+`node 1.js`
---
+++ day/1.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+
+const part1 = () => {
+	fs.readFile("../inputs/1.txt", "utf-8", function (err, data) {
+		data = data.split("\n");
+		data = data.map((x) => parseInt(x));
+		var output = 0;
+		var prev = undefined;
+		for (i in data) {
+			if (prev !== undefined && data[i] > prev) {
+				output += 1;
+			}
+			prev = data[i];
+		}
+		console.log(`Part 1 Answer: ${output}`);
+	});
+};
+
+const part2 = () => {
+	fs.readFile("../inputs/1.txt", "utf-8", function (err, data) {
+		data = data.split("\n");
+		data = data.map((x) => parseInt(x));
+		output = inputToOutput(data);
+		console.log(`Part 2 Answer: ${output}`);
+	});
+};
+
+const inputToOutput = (input) => {
+	var output = 0;
+	var prev = undefined;
+	for (var i = 0; i < input.length; i += 1) {
+		var window = input[i] + input[i + 1] + input[i + 2];
+
+		if (prev !== undefined && window > prev) {
+			output += 1;
+		}
+		prev = window;
+	}
+	return output;
+};
+
+part1();
+part2();
