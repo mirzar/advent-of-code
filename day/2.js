54a07b4a8fa5353808f66c454187badc9db5b499
mirzar <mirza@ziked.com>
2021-12-02 12:10:40 -0500

    Day 2

---
+++ day/2.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+
+const input = fs
+  .readFileSync('input-2.txt', 'utf8')
+  .toString()
+  .trim()
+  .split('\n')
+  .map(line => {
+    let [movement, amount] = line.split(' ');
+    amount = parseInt(amount, 10);
+    return {
+      movement,
+      amount,
+    };
+  });
+
+const part1 = () => {
+  let horizontal = 0;
+  let depth = 0;
+  for (let {movement, amount} of input) {
+    if (movement === 'up') {
+      depth -= amount;
+    } else if (movement === 'down') {
+      depth += amount;
+    } else if (movement === 'forward') {
+      horizontal += amount;
+    } else {
+      throw new Error(`Unknown movement: ${movement}`);
+    }
+  }
+  console.log(`Part 1 Solution is`, depth * horizontal);
+};
+
+const part2 = () => {
+  let horizontal = 0;
+  let depth = 0;
+  let aim = 0;
+  for (let {movement, amount} of input) {
+    if (movement === 'up') {
+      aim -= amount;
+    } else if (movement === 'down') {
+      aim += amount;
+    } else if (movement === 'forward') {
+      horizontal += amount;
+      depth += aim * amount;
+    } else {
+      throw new Error(`Unknown movement: ${movement}`);
+    }
+  }
+  console.log(`Part 2 Solution is`, depth * horizontal);
+};
+
+part1();
+part2();
