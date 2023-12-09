33ad9413118fc0f2180a1ad48d3c2998250882c1
mirzar <mirza@ziked.com>
2023-12-09 15:01:57 -0500

    Sync

---
+++ 2023/day9/solution.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('./input.txt', 'utf8').trimEnd();
+
+const solvePart1 = input => {
+  let sum = 0;
+  for (const line of input.split('\n')) {
+    let curr = line.match(/[-\d]+/g).map(Number);
+    const rows = [curr];
+    while (curr.some(Boolean)) {
+      const next = [];
+      for (let i = 0; i < curr.length - 1; i++) {
+        next.push(curr[i + 1] - curr[i]);
+      }
+      curr = next;
+      rows.push(next);
+    }
+    let carry = 0;
+    for (let i = rows.length - 2; i >= 0; i--) {
+      carry += rows[i].at(-1);
+    }
+    sum += carry;
+  }
+  console.log('Part One', sum);
+};
+solvePart1(input);
+
+const solvePart2 = input => {
+  let sum = 0;
+  for (const line of input.split('\n')) {
+    let curr = line.match(/[-\d]+/g).map(Number);
+    const rows = [curr];
+    while (curr.some(Boolean)) {
+      const next = [];
+      for (let i = 0; i < curr.length - 1; i++) {
+        next.push(curr[i + 1] - curr[i]);
+      }
+      curr = next;
+      rows.push(next);
+    }
+    let carry = 0;
+    for (let i = rows.length - 2; i >= 0; i--) {
+      carry = rows[i][0] - carry;
+    }
+    sum += carry;
+  }
+  console.log('Part Two', sum);
+};
+solvePart2(input);
