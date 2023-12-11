729b47ed4f2f64dc75dfa83aeb5a8b6598dfcda6
Mirza Rahman <mirza.rahman@affirm.com>
2023-12-11 11:50:20 -0500

    Day 11

---
+++ 2023/day11/solution.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('./input.txt', 'utf8').trimEnd();
+
+const solution = (input, expansion) => {
+  const img = input
+    .split('\n')
+    .map((line) => line.split('').map((char) => +(char === '#')));
+
+  const emptyRows = [];
+  for (let i = 0; i < img.length; i++) {
+    if (!img[i].some(Boolean)) {
+      emptyRows.push(i);
+    }
+  }
+
+  const emptyCols = [];
+  for (let j = 0; j < img[0].length; j++) {
+    if (!img.map((row) => row[j]).some(Boolean)) {
+      emptyCols.push(j);
+    }
+  }
+
+  const galaxies = [];
+  for (let i = 0; i < img.length; i++) {
+    for (let j = 0; j < img[i].length; j++) {
+      if (img[i][j]) {
+        galaxies.push([i, j]);
+      }
+    }
+  }
+
+  let sum = 0;
+  for (let i = 0; i < galaxies.length - 1; i++) {
+    const [r1, c1] = galaxies[i];
+    for (let j = i + 1; j < galaxies.length; j++) {
+      const [r2, c2] = galaxies[j];
+      const rows = [r1, r2].sort((a, b) => a - b);
+      const cols = [c1, c2].sort((a, b) => a - b);
+      let nCrosses = 0;
+      for (const r of emptyRows) {
+        if (rows[0] < r && r < rows[1]) {
+          nCrosses++;
+        }
+      }
+      for (const c of emptyCols) {
+        if (cols[0] < c && c < cols[1]) {
+          nCrosses++;
+        }
+      }
+      sum += rows[1] - rows[0] + cols[1] - cols[0] + nCrosses * (expansion - 1);
+    }
+  }
+  console.log(sum);
+}
+solution(input, 2);
+solution(input, 1000000);
